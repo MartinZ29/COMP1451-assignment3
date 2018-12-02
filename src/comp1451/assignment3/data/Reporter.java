d133d1a023c9e4d5e450b295a065250350cb427c
Martin Zhang <mtnz6748@gmail.com>
2018-12-02 15:40:11 -0800

    create methods in interface

--- src/comp1451/assignment3/data/Reporter.java
+++ src/comp1451/assignment3/data/Reporter.java
@@ -1,5 +1,7 @@
 package comp1451.assignment3.data;
 
+import java.util.HashMap;
+
 /**
  * 
  * @author Martin
@@ -7,5 +9,26 @@ package comp1451.assignment3.data;
  */
 public interface Reporter {
 
+	/**
+	 * Display information of accounts according to prefix in the records.
+	 * @param records
+	 * 				- is the accounts in the bank in HashMap
+	 * @param prefix
+	 * 				- is type of the account set for the account number
+	 */
+	void displayByCode(HashMap records, String prefix);
 	
+	/**
+	 * Display all accounts information in the records.
+	 * @param records
+	 *				 - is the accounts in the bank in HashMap
+	 */
+	void displayAllCodes(HashMap records);
+	
+	/**
+	 * Display all inactive accounts in the records
+	 * @param records
+	 * 				- is the accounts in the bank in HashMap
+	 */
+	void displayInactiveCodes(HashMap records);
 }
